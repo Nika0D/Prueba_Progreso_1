8c64224150e4c5aa96c0854da7b4fc331463cfe4
Laboratorio <laboratorio@udla-ec.int>
2023-10-24 14:31:44 -0500

    planteamiento de estreucturas

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
+int main()
 {
-    /* prueba de repositorio */
+    int numero1;
+    printf("ingrese un numero");
+    scanf("%d", numero1);
+    while ()
+    {
+        /* code */
+    }  
     return 0;
+}
+int numero_m(numero1){
+    int numero1, numero2, continuar=1;
+    do
+    {
+        printf("ingrese un numero");
+        scanf("%d", numero1);
+        printf("Si desea continuar escriba 1");
+        scanf("%d", numero1);
+        if (numero1 >= numero2)
+        {
+            /* code */
+        }
+        
+    } while (continuar==1);
+    return numero2;
 }
