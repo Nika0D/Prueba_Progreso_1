ba2fc150b0945a1d736e4b6d577844f6fbd63839
Laboratorio <laboratorio@udla-ec.int>
2023-10-24 14:49:37 -0500

    Evolucion del ejercicio

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+/*Escriba un programa que reciba como datos de entrada n números 
+enteros y regrese como dato de salida el mayor de estos números. 
+Debe utilizar el concepto de función.*/
 int main()
 {
     int numero1;
-    printf("ingrese un numero");
-    scanf("%d", numero1);
-    while ()
+    int continuar=1;
+    while (continuar==1)
     {
-        /* code */
-    }  
+        printf("ingrese un numero");
+        scanf("%d", &numero1);
+        int numero_m(res);
+        printf("Si desea continuar escriba 1");
+        scanf("%d", &continuar);
+    }
+    printf("El numero mayor es: %d\n", numero_m);
     return 0;
 }
 int numero_m(numero1){
-    int numero1, numero2, continuar=1;
-    do
+    int numero1, res = 0, continuar=1;
+    if (numero1 >= res)
     {
-        printf("ingrese un numero");
-        scanf("%d", numero1);
-        printf("Si desea continuar escriba 1");
-        scanf("%d", numero1);
-        if (numero1 >= numero2)
-        {
-            /* code */
-        }
-        
-    } while (continuar==1);
-    return numero2;
+        res=numero1; 
+    }
+    return res;
 }
