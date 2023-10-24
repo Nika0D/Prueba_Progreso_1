1e152dd38add733598d2c45cf7b953fc18a67111
Laboratorio <laboratorio@udla-ec.int>
2023-10-24 15:08:08 -0500

    Ejercicio resuelto

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -4,24 +4,25 @@ enteros y regrese como dato de salida el mayor de estos números.
 Debe utilizar el concepto de función.*/
 int main()
 {
-    int numero1;
+    int num, mayor;
     int continuar=1;
     while (continuar==1)
     {
         printf("ingrese un numero");
-        scanf("%d", &numero1);
-        int numero_m(res);
-        printf("Si desea continuar escriba 1");
+        scanf("%d", &num);
+        int numero_m(num);
+        mayor=numero_m(num);
+        printf("Si desea continuar escriba 1 sino escriba otro valor");
         scanf("%d", &continuar);
     }
-    printf("El numero mayor es: %d\n", numero_m);
+    printf("El numero mayor es: %d\n", mayor);
     return 0;
 }
-int numero_m(numero1){
-    int numero1, res = 0, continuar=1;
-    if (numero1 >= res)
+int numero_m(num){
+    int res = 0;
+    if (num >= res)
     {
-        res=numero1; 
+        res=num; 
     }
     return res;
 }
